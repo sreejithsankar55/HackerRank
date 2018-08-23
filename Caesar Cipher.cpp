f805bfb93841d0545a3fb58682e3f4c0ef650d77
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-08-23 14:50:35 +0530

    Update Caesar Cipher.cpp

--- Caesar Cipher.cpp
+++ Caesar Cipher.cpp
@@ -12,34 +12,36 @@ using namespace std ;
 
 int main()
 {
-	ll n,r,k;
-	cin>>n;
-	string s,t;
-	cin>>s;
-	cin>>k;
-	for(ll i = 0 ; i < n ; i++)
-	{
-		r=(int)(s[i]);
-		switch(s[i])
-		{
-			case 'a' ... 'z' : r-=96;
-							   r=(r+k)%26;
-							   if(r==0)
-								r=26;
-							   r+=96;
-							   s[i]=r;
-							   break;
-			case 'A' ... 'Z' : r-=64;
-							   r=(r+k)%26;
-							   if(r==0)
-								r=26;
-							   r+=64;
-							   s[i]=r;
-							   break;				
-							   
-		}
-		t+=s[i];
-	}
+    ll n,r,k;
+    cin>>n;
+    string s,t;
+    cin>>s;
+    cin>>k;
+    for(ll i = 0 ; i < n ; i++)
+    {
+        r=(int)(s[i]);
+        switch(s[i])
+        {
+        case 'a' ... 'z' :
+            r-=96;
+            r=(r+k)%26;
+            if(r==0)
+                r=26;
+            r+=96;
+            s[i]=r;
+            break;
+        case 'A' ... 'Z' :
+            r-=64;
+            r=(r+k)%26;
+            if(r==0)
+                r=26;
+            r+=64;
+            s[i]=r;
+            break;
+
+        }
+        t+=s[i];
+    }
     cout<<t;
     return 0;
 }
