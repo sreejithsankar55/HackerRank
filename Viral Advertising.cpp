b90c65125dad7a702d48756e129d9866dc620f4f
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-08-23 17:37:12 +0530

    Create Viral Advertising.cpp

---
+++ Viral Advertising.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define pb  push_back
+#define mp  make_pair
+#define ff  first
+#define ss  second
+using namespace std ;
+
+int main()
+{
+    ll n,s=5,l=2,c=2;
+    cin>>n;
+    for(ll i = 2 ; i <=n ;i++)
+    {
+        s=l*3;
+        l=s/2;
+        c=c+l;
+    }
+    cout<<c;
+    
+    return 0;
+}
