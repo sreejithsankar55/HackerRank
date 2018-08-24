135f00543c51f07f425db105305bbe96ef32909d
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-08-24 14:38:28 +0530

    Create Funny String.cpp

---
+++ Funny String.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define pb  push_back
+#define mp  make_pair
+#define ff  first
+#define ss  second
+using namespace std ;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        ll f=1;
+        ll size=s.size();
+        vector < ll > v1,v2;
+        for(ll i = 1 ; i < size ; i++)
+            v1.pb(abs(s[i]-s[i-1]));
+        for(ll i = size-1 ; i > 0 ; i--)
+            v2.pb(abs(s[i]-s[i-1]));
+        
+       (v1 == v2)?  cout << "Funny\n" : cout << "Not Funny\n";    
+    }
+     
+    return 0;
+}
